5069fd4dcda9a1371c0b5ae310275e66acad726e
chuks umenwa <chuksumenwa@yahoo.com>
2021-08-24 13:30:33 +0100

    arrays

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import "fmt"
 func main()  {
 	fmt.Println("Here We go")
 	anything("Chuks",20)
+	b,t,s := fruits( "Chuks has", 25,"oranges")
+	fmt.Println(b,t,s)
+	 newAge:=calculateAge(25,10)
+	 fmt.Println(newAge)
+	 array()
 
 }
 func anything(name string, age int) (string, int) {
@@ -13,4 +18,20 @@ func anything(name string, age int) (string, int) {
 	fmt.Println("I am", age)
 
 	return name, age
+}
+func fruits (name string, number int, name2 string)(string,int,string){
+	number= number *10
+	return  name, number,name2
+
+}
+func calculateAge(currentAge,numOfYears int)int{
+	result:= currentAge + numOfYears
+	return result
+}
+func array(){
+	arr :=[]int{17,26,10,80}
+	for idx, val:= range arr{
+		fmt.Println(idx,val)
+	}
+
 }
